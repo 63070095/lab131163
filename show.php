03f9aed9b23743eb78bf01c969bbe807f390232a
63070095 <73688535+63070095@users.noreply.github.com>
2020-11-13 15:30:01 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,3 +1,4 @@
+<html>
 <head>
 <title>ITF Lab</title>
 </head>
@@ -12,7 +13,35 @@ if (mysqli_connect_errno($conn))
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <div class="container">
-<table class="table table-dark table-hover">
+  <h2>Borderless Table</h2>
+  <p>The .table-borderless class removes borders from the table:</p>            
+  <table class="table table-borderless">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 <table width="600" border="1">
   <tr>
     <th width="100"> <div align="center">Name</div></th>
@@ -32,8 +61,6 @@ while($Result = mysqli_fetch_array($res))
 }
 ?>
 </table>
-</table>
-</div>
 <?php
 mysqli_close($conn);
 ?>
