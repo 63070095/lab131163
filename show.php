05ab6c11138c787804d9a3ed3d3e16822ddb0b5b
63070095 <73688535+63070095@users.noreply.github.com>
2020-11-13 15:32:06 +0700

    Update show.php

--- show.php
+++ show.php
@@ -12,42 +12,14 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<div class="container">
-  <h2>Borderless Table</h2>
-  <p>The .table-borderless class removes borders from the table:</p>            
+<div class="container">           
   <table class="table table-borderless">
-    <thead>
+    <table width="600" border="1">
       <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-<table width="600" border="1">
-  <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
     <th width="150"> <div align="center">Link </div></th>
-  </tr>
+      </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
@@ -60,7 +32,9 @@ while($Result = mysqli_fetch_array($res))
 <?php
 }
 ?>
-</table>
+    </table>
+  </table>
+</div>
 <?php
 mysqli_close($conn);
 ?>
