15873f668bca5b82c878cf1be866fb242188f884
63070095 <73688535+63070095@users.noreply.github.com>
2020-11-15 17:23:36 +0700

    Update show.php

--- show.php
+++ show.php
@@ -38,7 +38,7 @@ while($Result = mysqli_fetch_array($res))
   </table>
 </div>
 <div class="container">
-    <table class="table table-hover">
+    <table class="table">
         <table width="600" border="1">
             <tr>
             <td><div align="center"><a href="form.html">เพิ่ม</a></div></td>
