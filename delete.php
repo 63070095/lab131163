d5f27cd9d360aedb8a489161020831bb9743cd7c
63070095 <73688535+63070095@users.noreply.github.com>
2020-11-15 18:52:41 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -13,7 +13,7 @@ $comment = $_POST['comment'];
 $link = $_POST['link'];
 
 
-$sql = "DELETE FROM guestbook WHERE $name";
+$sql = "DELETE FROM guestbook WHERE id='$id";
 
 
 if (mysqli_query($conn, $sql)) {
