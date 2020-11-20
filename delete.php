f6fe0000787cfcc2700d74310c2d9b50b27c7e45
63070095 <73688535+63070095@users.noreply.github.com>
2020-11-20 14:17:39 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -15,7 +15,8 @@ $sql = $sql = "DELETE FROM guestbook WHERE name='$name'";
 $sql = "DELETE FROM guestbook WHERE Comment='$comment'";
 
 if (mysqli_query($conn, $sql)) {
-    echo "Delete successfully! CONGRATULATION!";
+    echo "Delete successfully";
+    echo '<a href="https://nidaweb.azurewebsites.net/show.php"> click here to open first page </a>';
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
