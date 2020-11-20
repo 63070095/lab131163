d598c53c87432c805e6bf13fa0aa57dab0571448
63070095 <73688535+63070095@users.noreply.github.com>
2020-11-20 12:27:07 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -4,20 +4,19 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'nidaitf.mysql.database.azure.com', 'it63070095@nidaitf', 'CDBhlh62', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+    die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
 }
 
 
 $name = $_POST['name'];
 $comment = $_POST['comment'];
-$link = $_POST['link'];
 
 
-$sql = "UPDATE guestbook WHERE id=2;
+$sql = "UPDATE guestbook SET Comment='$comment' WHERE Name='$name' ";
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "Delete successfully";
+    echo "New record created successfully! CONGRATULATION!";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
