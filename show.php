b308ea35def3f2e274cd70a9af59a29cfcd181b1
63070095 <73688535+63070095@users.noreply.github.com>
2020-11-20 12:23:41 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,6 +1,16 @@
 <html>
 <head>
-<title>ITF Lab Database</title>
+<title>˗ˏˋITF LABˎˊ˗</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  p.ex1 {
+    margin-top: 25px;
+  }
+  </style>
 </head>
 <body>
 <?php
@@ -8,59 +18,38 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'nidaitf.mysql.database.azure.com', 'it63070095@nidaitf', 'CDBhlh62', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+    die('Failed to connect to MySQL PLEASE TRY AGAIN !: '.mysqli_connect_error());
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
-if($_REQUEST['ID'] != "") //ถ้า ค่า del_id ไม่เท่ากับค่าว่างเปล่า
-{
-$ID = $_REQUEST['ID'];
-$sql_del = "DELETE from guestbook where id = '$ID';";
-mysql_query($sql_del) or die(mysql_error());
-echo "ลบข้อมูล ID $del_id เรียบร้อยแล้ว";
-}
 ?>
-<div class="container">           
-  <table class="table table-hover">
-    <table width="600" border="1">
-      <tr>
-    <th width="100"> <div align="center">ID</div></th>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-    <th width="150"> <div align="center">Edit </div></th>
-    <th width="150"> <div align="center">Delete </div></th>
-      </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?=$row_show['ID']?></td>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-    <td><div align="center"><a href="edit.php">แก้ไข</a></div></td>
-    <td><div align="center"><a href="delete.php?ID=$row[0]">ลบ</a></div></td>
-  </tr>
-<?php
-}
-?>
-    </table>
-  </table>
-</div>
+
 <div class="container">
-    <table class="table table-dark">
-        <table width="600" border="1">
-            <tr>
-            <td><div align="center"><a href="form.html">เพิ่ม</a></div></td>
-            </tr>
-        </table>
-    </table>
+    <div class="form">
+        <p class="ex1"><h1><center>꒰FORM =͟͟͞͞♡</center></h1></p>
+    </div>
+  <table width="600" border="1" class="table table-dark table-striped" class="center" class="ex1">
+    <tr class="info">
+      <th width="200"> <div align="center">Name</div></th>
+      <th width="350"> <div align="center">Comment </div></th>
+      <th width="50"> <div align="center">Action</div></th>
+    </tr>
+  <?php
+  while($Result = mysqli_fetch_array($res))
+  {
+  ?>
+    <tr>
+      <td><center><?php echo $Result['Name'];?></center></td>
+      <td><center><?php echo $Result['Comment'];?></center></td>
+      <td><center><a href="form edit.html"><input type="submit" value="EDIT" class="btn btn-warning"></a>&nbsp;&nbsp;<a href="form delete.html"><input type="submit" value="DELETE"  class="btn btn-danger"></a></center></td>
+    </tr>
+  <?php
+  }
+  ?>
+  </table>
+  <center><a href="form insert.html"><input type="submit" value="INSERT" class="btn btn-info"></a></center>
+  <?php
+  mysqli_close($conn);
+  ?>
 </div>
-
-
-<?php
-mysqli_close($conn);
-?>
 </body>
 </html>
