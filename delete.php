5367c2cb4d1efda3c73d7bb00b51dde314d6b5a1
63070095 <73688535+63070095@users.noreply.github.com>
2020-11-20 12:27:40 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,18 +4,19 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'nidaitf.mysql.database.azure.com', 'it63070095@nidaitf', 'CDBhlh62', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+    die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
 }
 
 
-$ID = $_REQUEST['ID'];
+$name = $_POST['name'];
+$comment = $_POST['comment'];
 
 
-$sql = "DELETE FROM guestbook WHERE ID='$ID'";
+$sql = $sql = "DELETE FROM guestbook WHERE name='$name'";
+$sql = "DELETE FROM guestbook WHERE Comment='$comment'";
 
 if (mysqli_query($conn, $sql)) {
-    echo "Delete successfully";
-    echo "window.location = 'Show.php';";
+    echo "New record created successfully! CONGRATULATION!";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
