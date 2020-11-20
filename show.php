3187b35a45f0a20ea14808c2f47da4cd4cff39bb
63070095 <73688535+63070095@users.noreply.github.com>
2020-11-20 11:12:09 +0700

    Update show.php

--- show.php
+++ show.php
@@ -16,6 +16,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   <table class="table table-hover">
     <table width="600" border="1">
       <tr>
+    <th width="100"> <div align="center">ID</div></th>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
     <th width="150"> <div align="center">Link </div></th>
